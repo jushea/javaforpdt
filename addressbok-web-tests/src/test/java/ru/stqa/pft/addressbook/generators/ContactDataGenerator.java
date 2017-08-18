13aa8d1be4d581b2c5fb88c0b91d057d5a09ea19
Yulia Shepeleva <yshepeleva@issart.com>
2017-08-18 23:53:31 +0600

    закрытие файлов после использования

--- addressbok-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbok-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -49,19 +49,19 @@ public class ContactDataGenerator {
         XStream xstream = new XStream();
         xstream.processAnnotations(ContactData.class);
         String xml = xstream.toXML(contacts);
-        Writer writer = new FileWriter(file);
-        writer.write(xml);
-        writer.close();
+        try(Writer writer = new FileWriter(file)) {
+            writer.write(xml);
+        }
     }
 
     private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
-        Writer writer = new FileWriter(file);
-        for(ContactData contact: contacts) {
-            writer.write(String.format("%s:%s:%s:%s:%s:%s:%s:%s:%s\n", contact.getFirstname(), contact.getSurname(),
-                    contact.getStreet(), contact.getPhoneHome(), contact.getMobile(), contact.getPhoneWork(),
-                    contact.getEmail1(), contact.getEmail2(), contact.getEmail3()));
+        try(Writer writer = new FileWriter(file)) {
+            for(ContactData contact: contacts) {
+                writer.write(String.format("%s:%s:%s:%s:%s:%s:%s:%s:%s\n", contact.getFirstname(), contact.getSurname(),
+                        contact.getStreet(), contact.getPhoneHome(), contact.getMobile(), contact.getPhoneWork(),
+                        contact.getEmail1(), contact.getEmail2(), contact.getEmail3()));
+            }
         }
-        writer.close();
     }
 
     private List<ContactData> generateContacts(int count) {
--- addressbok-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbok-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -22,17 +22,18 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTest extends TestBase {
     @DataProvider
     public Iterator<Object[]> validContacts() throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
-        String xml = "";
-        String line = reader.readLine();
-        while(line != null) {
-            xml += line;
-            line = reader.readLine();
+        try(BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")))) {
+            String xml = "";
+            String line = reader.readLine();
+            while(line != null) {
+                xml += line;
+                line = reader.readLine();
+            }
+            XStream xstream = new XStream();
+            xstream.processAnnotations(ContactData.class);
+            List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+            return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
         }
-        XStream xstream = new XStream();
-        xstream.processAnnotations(ContactData.class);
-        List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
-        return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
     }
 
     @Test(dataProvider = "validContacts")
@@ -43,7 +44,6 @@ public class ContactCreationTest extends TestBase {
         app.contact().create(contact);
         assertThat(app.contact().getContactCount(), equalTo(before.size()+1));
         Contacts after = app.contact().all();
-
         assertThat(after, equalTo(
                 before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
     }

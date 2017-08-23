98abf8db8810b86fd5d83629c4ae70c977e1b058
Yulia Shepeleva <yshepeleva@issart.com>
2017-08-23 11:09:20 +0600

    сравнение данных из БД

--- addressbok-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbok-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -11,20 +11,21 @@ public class GroupModificationTests extends TestBase {
 
     @BeforeMethod
     public void ensurePrecondition() {
-        app.goTo().groupPage();
-        if(app.group().all().size() == 0) {
+        if(app.db().groups().size() == 0) {
+            app.goTo().groupPage();
             app.group().create(new GroupData().withName("test1").withHeader("test2").withFooter("test3"));
         }
     }
 
     @Test
     public void testGroupModification() {
-        Set<GroupData> before = app.group().all();
+        Set<GroupData> before = app.db().groups();
         GroupData modifiedGroup = before.iterator().next();
         GroupData group = new GroupData().withId(modifiedGroup.getId()).withName("test1").withHeader("test2").withFooter("test3");
+        app.goTo().groupPage();
         app.group().modify(group);
         Assert.assertEquals(app.group().count(), before.size());
-        Set<GroupData> after = app.group().all();
+        Set<GroupData> after = app.db().groups();
 
         before.remove(modifiedGroup);
         before.add(group);

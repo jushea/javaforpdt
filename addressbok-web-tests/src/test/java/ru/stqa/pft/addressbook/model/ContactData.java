2a512b83444927de69367c4c7c3c912f948416f6
Yulia Shepeleva <yshepeleva@issart.com>
2017-08-23 10:39:03 +0600

    условия отбора данных

--- addressbok-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbok-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -2,32 +2,70 @@ package ru.stqa.pft.addressbook.model;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 
 @XStreamAlias("contact")
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
     @XStreamOmitField
-    private int id = Integer.MAX_VALUE;
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "firstname")
     private  String firstname;
+
+    @Column(name = "lastname")
     private  String surname;
+
+    @Column(name = "address")
+    @Type(type = "text")
     private  String street;
+
+    @Column(name = "home")
+    @Type(type = "text")
     private  String phoneHome;
+
+    @Column(name = "mobile")
+    @Type(type = "text")
     private  String mobile;
+
+    @Column(name = "work")
+    @Type(type = "text")
     private  String phoneWork;
+
+    @Column(name = "email")
+    @Type(type = "text")
     private  String email1;
+
+    @Column(name = "email2")
+    @Type(type = "text")
     private  String email2;
+
+    @Column(name = "email3")
+    @Type(type = "text")
     private  String email3;
+
+    @Transient
     private  String allPhones;
+
+    @Transient
     private  String allEmail;
-    private File photo;
+
+    @Column(name = "photo")
+    @Type(type = "text")
+    private String photo;
 
     public File getPhoto() {
-        return photo;
+        return new File(photo);
     }
 
     public ContactData withPhoto(File photo) {
-        this.photo = photo;
+        this.photo = photo.getPath();
         return this;
     }
 
